ce1d666bbee83411b1bc9fa91f7b27e2d193cbd7
MrPotatorust <bumko96@gmail.com>
2024-07-28 23:48:04 +0200

    Made scrapeddata, tableinfo models

--- website/scraper_integration/models.py
+++ website/scraper_integration/models.py
@@ -1,10 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Scraped_data(models.Model):
 
-    def __str__(self):
-        return 
-    
-    
+class TableInfo(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    creation_date = models.DateField()
+
+
+class ScrapedData(models.Model):
+
+    table = models.ForeignKey(TableInfo, on_delete=models.CASCADE)
+    price = models.FloatField(null=True)
+    price_per_sqm = models.FloatField(null=True)
+    area = models.FloatField(null=True)
+    location = models.CharField(max_length=100)
+    type_of_property = models.CharField(max_length=30)
